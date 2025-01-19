a368f55049b73c91a2382e4c6b329929ef36177e
Ito Yamato <daxiangy147@gmail.com>
2025-01-19 21:46:59 +0900

    ボールの初期速度ベクトルにランダム性を持たせた

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -10,12 +10,27 @@ public class Ball : MonoBehaviour
     void Start()
     {
         myRigid = this.GetComponent<Rigidbody>();
-        myRigid.AddForce((transform.forward + transform.right) * speed, ForceMode.VelocityChange);
+        // ランダムな方向を生成
+        Vector3 randomDirection = GetRandomDirection();
+        // ボールにランダムな方向の力を加える
+        myRigid.AddForce(randomDirection * speed, ForceMode.VelocityChange);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    // x-z 平面上のランダムな方向を返すメソッド
+    private Vector3 GetRandomDirection()
+    {
+        // ランダムな角度を生成 (0 ~ 360度)
+        float randomAngle = Random.Range(0f, 360f);
+
+        // ランダムな角度を使って単位ベクトルを計算 (y成分は0に固定, z成分は1に固定)
+        float x = Mathf.Cos(randomAngle * Mathf.Deg2Rad);
+
+        return new Vector3(x, 0f, 1f).normalized; // 正規化して一定の大きさを維持
     }
 }
