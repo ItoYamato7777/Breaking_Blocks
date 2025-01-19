ae390ea315fada98826c8fa82e689e4de41b0c00
ninparo <ogretotem.game@gmail.com>
2025-01-19 21:44:12 +0900

    Block Destroy Effect

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,20 +1,38 @@
+using System.Collections;
 using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    bool destroy = false;
+    Vector3 scale;
+    Vector3 firstScale;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        scale = transform.localScale;
+        firstScale = transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (destroy) {
+            scale.x -= firstScale.x * Time.deltaTime;
+            scale.z -= firstScale.z * Time.deltaTime;
+            this.transform.localScale = scale;
+        }
     }
     private void OnCollisionEnter(Collision collision)
     {
+        if (!destroy) {
+            StartCoroutine("Effecter");
+        }
+    }
+
+    IEnumerator Effecter() {
+        destroy = true;
+        this.GetComponent<BoxCollider>().isTrigger = true;
+        yield return new WaitForSeconds(1.0f);
         Destroy(this.gameObject);
     }
 }
